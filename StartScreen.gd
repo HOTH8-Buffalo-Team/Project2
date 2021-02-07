a5c773959d2e95e13d61b0ee52609cdba9c8cc85
Evan Cheung <evan.d.cheung@gmail.com>
2021-02-07 13:00:22 -0800

    solids
    yo we got solids?
    particles even?

--- StartScreen.gd
+++ StartScreen.gd
@@ -10,13 +10,15 @@ var selected = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func start():
 	running = true
+	$Particles.emitting = true
 
 func stop():
 	running = false
+	$Particles.emitting = false
 
 
 func checkInputs():
